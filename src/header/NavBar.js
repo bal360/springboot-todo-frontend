969f8f54ca6627590ff9e93d906f076de4816abf
Blake A. Long <blake.a.long@gmail.com>
2021-01-26 20:37:23 -0700

    minor additions to NavBar

--- src/header/NavBar.js
+++ src/header/NavBar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { FcTodoList } from 'react-icons/fc';
+
+
 
 const NavBar = () => {
   return (
     <>
-      NavBar
+      <Navbar>
+        <Navbar.Brand><FcTodoList size={30} /></Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link>Todos</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link>Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   )
 }
