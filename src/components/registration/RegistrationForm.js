9a2273ce0ece06a8329988508e9bcfeb6105b62c
Blake A. Long <blake.a.long@gmail.com>
2021-01-30 14:30:37 -0700

    firstName, lastName, and username validations working

--- src/components/registration/RegistrationForm.js
+++ src/components/registration/RegistrationForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Form } from 'react-bootstrap';
+import { Col, Button, Row, Form } from 'react-bootstrap';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import LogoHeader from '../FormHeader';
@@ -19,32 +19,58 @@ const StyledError = styled.p`
 const RegistrationForm = props => {
   const { register, handleSubmit, errors, watch } = useForm()
 
-  // const onSubmit = data => {
+  const onSubmit = data => {
+    console.log(data)
   //   const { username, password, firstName, lastName, email } = data
   //   // fetch function coming
   //   // registerUser() 
-  // }
+  }
 
   return (
     <>
       <LogoHeader text={"Register New Account"} />
       <StyledRow>
-        <StyledForm>
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control 
+                type="text"
+                name="firstName"
+                ref={register({
+                  required: "First name required"
+                })}
+              />
+              {errors.firstName && <StyledError>{errors.firstName.message}</StyledError>}
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control 
+                type="text"
+                name="lastName"
+                ref={register({
+                  required: "Last name required"
+                })}
+              />
+              {errors.firstName && <StyledError>{errors.firstName.message}</StyledError>}
+            </Form.Group>
+          </Form.Row>
           <Form.Group>
             <Form.Label>Username</Form.Label>
-            <Form.Control 
-              type="text"
-              name="username"
-              ref={register({
-                required: "Username is required",
-                minLength: {
-                  value: 5,
-                  message: "Must be at least 5 characters long"
-                }
-              })}
-            />
-            {errors.username && <StyledError>{errors.username.message}</StyledError>}
+                <Form.Control 
+                  type="text"
+                  name="username"
+                  ref={register({
+                    required: "Username is required",
+                    minLength: {
+                      value: 5,
+                      message: "Must be at least 5 characters long"
+                    }
+                  })}
+                />
+                {errors.username && <StyledError>{errors.username.message}</StyledError>}
           </Form.Group>
+          <Button type="submit">Submit</Button>
         </StyledForm>
       </StyledRow>
     </>
