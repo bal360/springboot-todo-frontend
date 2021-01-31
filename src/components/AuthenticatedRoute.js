d049e4ed701dd3f46ac702fffd74bd6aa64478be
Blake A. Long <blake.a.long@gmail.com>
2021-01-31 13:12:45 -0700

    switched to class component for now in AuthenticatedRoute since interceptors are resetting on refresh

--- src/components/AuthenticatedRoute.js
+++ src/components/AuthenticatedRoute.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { isLoggedIn, setupAxiosInterceptors } from '../auth/AuthenticationService';
 
-const AuthenticatedRoute = props => {
-  // componentDidMount() {
-  //   // setupAxiosInterceptors()
-  // }
-
-  if (isLoggedIn()) {
-    return <Route {...props} />
-  } else {
-    return <Redirect to="/login" />
+class AuthenticatedRoute extends React.Component {
+  componentDidMount() {
+    setupAxiosInterceptors()
+  }
+  render() {
+    if (isLoggedIn()) {
+      return <Route {...this.props} />
+    } else {
+      return <Redirect to="/login" />
+    }
   }
 }
 
