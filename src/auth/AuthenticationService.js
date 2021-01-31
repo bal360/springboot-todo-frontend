14fe0ed420ac7510cad7b754be020d85ecf080cd
Blake A. Long <blake.a.long@gmail.com>
2021-01-30 21:54:45 -0700

    AuthenticationService and all necessary functions defined

---
+++ src/auth/AuthenticationService.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const sessionStorageUsername = 'authenticatedUser'
+const LOC_URL = 'http://www.localhost:5000.com'
+
+export const executeJwtAuthService = (username, password) => {
+  return axios.post(`${LOC_URL}/authenticate`, {
+    username, 
+    password
+  })
+}
+
+const createJwtToken = token => {
+  sessionStorage.setItem('USER_TOKEN', `Bearer ${token}`)
+  return `Bearer ${token}`
+}
+
+export const registerJwtLogin = (username, token) => {
+  sessionStorage.setItem(sessionStorageUsername, username)
+  setupAxiosInterceptors(createJwtToken(token))
+}
+
+export const isLoggedIn = () => {
+  let user = sessionStorage.getItem(sessionStorageUsername)
+  return user !== null ? true : false
+}
+
+export const setupAxiosInterceptors = () => {
+  axios.interceptors.request.use((config) => {
+    if (isLoggedIn()) {
+      config.headers.authorization = sessionStorage.getItem('USER_TOKEN')
+    }
+    return config
+  })
+}
+
