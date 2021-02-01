3254b8ee6fe6bd17e0311af7685cf6c0c40820ef
Blake A. Long <blake.a.long@gmail.com>
2021-01-31 21:56:00 -0700

    completed form inputs with register for validations set up and FormButton

--- src/components/todos/TodoForm.js
+++ src/components/todos/TodoForm.js
@@ -5,15 +5,38 @@ import moment from 'moment';
 import styled from 'styled-components';
 import { getTodo, createTodo, updateTodo } from '../../api/todo/TodoService';
 import { getLoggedInUser } from '../../auth/AuthenticationService';
+import FormButton from '../form-components/FormButton';
 
 const StyledRow = styled(Row)`
-  
+  justify-content: center;
 `
 
 const TodoForm = props => {
+  const { register, handleSubmit, errors } = useForm()
+
   return (
     <StyledRow>
-      Form Here
+      <Form>
+        <Form.Group>
+          <Form.Label>Todo</Form.Label>
+          <Form.Control 
+            type="text"
+            name="description"
+            ref={register({ required: "Description required" })}
+          />
+          {errors.description && <p>{errors.description.message}</p>}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Date to be Completed</Form.Label>
+          <Form.Control 
+            type="text"
+            name="targetDate"
+            ref={register({ required: "Date required" })}
+          />
+          {errors.targetDate && <p>{errors.targetDate.message}</p>}
+        </Form.Group>
+        <FormButton text={"Save"} />
+      </Form>
     </StyledRow>
   )
 }
