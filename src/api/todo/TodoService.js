399bb5ef8d1e1ae277d42d265a7270d88edf34cc
Blake A. Long <blake.a.long@gmail.com>
2021-01-31 23:26:34 -0700

    remove parentheses from getAllTodos parameter

--- src/api/todo/TodoService.js
+++ src/api/todo/TodoService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'http://www.localhost:5000/users'
 
-export const getAllTodos = (username) => {
+export const getAllTodos = username => {
   return axios.get(`${BASE_URL}/${username}/todos`)
 }
 
